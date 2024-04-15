ea820f8ca20400d28a66150f9ef7f4e2b0c82caf
Alex Cardona <42.alex.cardona@gmail.com>
2024-04-15 19:37:15 +0200

    day00-ex00: refactor: ading an infinit loop to avoid constant restarts

--- day00/ex01/main.c
+++ day00/ex01/main.c
@@ -5,5 +5,7 @@ int main( void )
 	//objectiv: set the port A0 to 1, all the others to 0;
 	DDRB = 1 << PB0; //set the pin A0 as an output pin (others are input by default)
 	PORTB = 1 << PB0; // set the output value of A0 to 1
+	while (1)
+	{}
 	return (0);
 }
