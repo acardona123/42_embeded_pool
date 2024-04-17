dc512d7348846727432a4f0bff9ffabb5f620f17
Alex Cardona <42.alex.cardona@gmail.com>
2024-04-17 11:35:20 +0200

    day01:ex00: fix: avoid random value

--- day01/exercises/ex00/main.c
+++ day01/exercises/ex00/main.c
@@ -1,23 +1,26 @@
 # include "main.h"
 
+#define NUMBER_OF_CYCLES_PER_LOOP	4
+#define TARGET_FREQUENCY			0.5f
+#define CORRECTOR					10
+
+static const unsigned long long int target_cpt = F_CPU / NUMBER_OF_CYCLES_PER_LOOP * TARGET_FREQUENCY / CORRECTOR;
 
 int main( void )
 {
-
-	
 	DDRB = 1 << D2;
 	PORTB = 0;
 
 	unsigned long long int cpt;
 	cpt = 0;
-	while (1) //
+	while (1)
 	{
-		if (cpt == 190000) //
+		if (cpt == target_cpt)
 		{
 			PORTB ^= (1 << PB1);
 			cpt = 0;
 		}
-		++cpt; // 1 cycle
+		++cpt;
 	}
 	return (0);
 }
