ce078be320c5b443ec3f259c420a07dcd45936f4
Alex Cardona <42.alex.cardona@gmail.com>
2024-04-23 21:52:57 +0200

    day06_ex02: refactor

--- day06/exercises/ex02/main.h
+++ day06/exercises/ex02/main.h
@@ -2,9 +2,8 @@
 # define MAIN_H
 
 # include "tools.h"
+#include "rgb_leds.h"
 
 void wheel(uint8_t pos);
-void init_rgb();
-void set_rgb(uint8_t r, uint8_t g, uint8_t b);
 
 #endif
---
+++ day06/exercises/ex02/rgb_leds.h
@@ -0,0 +1,9 @@
+#ifndef RGB_LEDS_H
+# define RGB_LEDS_H
+
+#include "tools.h"
+
+void init_rgb();
+void set_rgb(uint8_t r, uint8_t g, uint8_t b);
+
+#endif
